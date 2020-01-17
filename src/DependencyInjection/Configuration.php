22cdc98df7c04dc8b968d7b543805c8d98348cd5
yanis <yanis@ipedis.com>
2020-01-17 11:06:44 +0400

    rollback to rabbit namespace

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -11,7 +11,7 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('ipedis_rabbit');
+        $treeBuilder = new TreeBuilder('rabbit');
 
         $treeBuilder->getRootNode()
             ->children()
