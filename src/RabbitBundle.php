b0e14b12d6641762b47036acee06590b54838c89
Tejas <tejas@ipedis.com>
2022-02-02 15:32:30 +0530

    added return type ans strong types

--- src/RabbitBundle.php
+++ src/RabbitBundle.php
@@ -5,11 +5,12 @@ namespace Ipedis\Bundle\Rabbit;
 
 
 use Ipedis\Bundle\Rabbit\DependencyInjection\RabbitExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class RabbitBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         if ($this->extension === null) {
             $this->extension = new RabbitExtension();
